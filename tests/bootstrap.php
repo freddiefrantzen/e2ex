7b46f7b58300e71c1ad3a96955401034ce782a48
Freddie Frantzen <freddie@freddiefrantzen.com>
2013-03-14 02:40:32 +0000

    updated documentation

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+/*
+ * This file is part of the E2EX package.
+ *
+ * (c) Freddie Frantzen <freddie@freddiefrantzen.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require __DIR__ . '/../vendor/autoload.php';
